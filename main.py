929dba8d13fcd2199e7c1f4a527e97249f013622
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-11 16:56:58 +0700

    Add an output model

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Any
 
 from models.item import Item
 from models.user import UserIn
+from models.user import UserOut
 
 app = FastAPI()
 
@@ -36,6 +37,13 @@ async def read_items() -> Any:
   ]
 
 # Return the same input data
+"""
 @app.post("/users", response_model = UserIn)
 async def create_users(user: Annotated[UserIn, Body(embed = True)]):
+  return user
+"""
+
+# Add an output model
+@app.post("/users", response_model = UserOut)
+async def create_user(user: Annotated[UserIn, Body(embed = True)]):
   return user
--- models/user.py
+++ models/user.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
+from pydantic import Field
 from pydantic import EmailStr
 
 class UserIn(BaseModel):
-  username: str
-  password: str
-  email: EmailStr
-  full_name: str | None = None
+  username: str = Field(examples = ["USER"])
+  password: str = Field(examples = ["Aa@123456"])
+  email: EmailStr = Field(examples = ["user@example.com"])
+  full_name: str | None = Field(default = None, examples = ["USER"])
+
+class UserOut(BaseModel):
+  username: str = Field(examples = ["USER"])
+  email: EmailStr = Field(examples = ["user@example.com"])
+  full_name: str | None = Field(default = None, examples = ["USER"])
