51a9be1cdf73068ae5bdde7998dcbde427de9752
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-11 16:49:38 +0700

    Return the same input data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from typing import Any
 
 from models.item import Item
+from models.user import UserIn
 
 app = FastAPI()
 
@@ -33,3 +34,8 @@ async def read_items() -> Any:
     Item(name = "Portal Gun", price=42.0),
     Item(name = "Plumbus", price=32.0)
   ]
+
+# Return the same input data
+@app.post("/users", response_model = UserIn)
+async def create_users(user: Annotated[UserIn, Body(embed = True)]):
+  return user
